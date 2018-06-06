21d1869f97f2bccb26d587a4107caa81b8696b73
damiano corrieri <dam.corr89@hotmail.com>
2018-06-06 17:42:00 +0200

    added another method

--- MyFirstGitHubApp/MyFirstGitHubApp/Program.cs
+++ MyFirstGitHubApp/MyFirstGitHubApp/Program.cs
@@ -4,6 +4,9 @@ namespace MyFirstGitHubApp
 {
     class Program
     {
+        static void anotherAuxMeth() {
+            Console.WriteLine("Sorry for having bothered you");
+                }
         static void auxMeth()
         {
             Console.WriteLine("writing something");
@@ -11,6 +14,7 @@ namespace MyFirstGitHubApp
         static void Main(string[] args)
         {
             auxMeth();
+            anotherAuxMeth();
             Console.ReadLine();
         }
     }
